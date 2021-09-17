a9d241e5505cf9cb73d0fbce2b86914441dbafc9
Aleksandr-Bondarenko <slenderman.dnp@gmail.com>
2021-09-17 18:44:04 +0300

    Updates

--- src.7caad0cb.js
+++ src.7caad0cb.js
@@ -1,8 +1,8 @@
 parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcelRequire,u="function"==typeof require&&require;function f(t,n){if(!r[t]){if(!e[t]){var i="function"==typeof parcelRequire&&parcelRequire;if(!n&&i)return i(t,!0);if(o)return o(t,!0);if(u&&"string"==typeof t)return u(t);var c=new Error("Cannot find module '"+t+"'");throw c.code="MODULE_NOT_FOUND",c}p.resolve=function(r){return e[t][1][r]||r},p.cache={};var l=r[t]=new f.Module(t);e[t][0].call(l.exports,p,l,l.exports,this)}return r[t].exports;function p(e){return f(p.resolve(e))}}f.isParcelRequire=!0,f.Module=function(e){this.id=e,this.bundle=f,this.exports={}},f.modules=e,f.cache=r,f.parent=o,f.register=function(r,t){e[r]=[function(e,r){r.exports=t},{}]};for(var c=0;c<t.length;c++)try{f(t[c])}catch(e){i||(i=e)}if(t.length){var l=f(t[t.length-1]);"object"==typeof exports&&"undefined"!=typeof module?module.exports=l:"function"==typeof define&&define.amd?define(function(){return l}):n&&(this[n]=l)}if(parcelRequire=f,i)throw i;return f}({"GjNk":[function(require,module,exports) {
 console.log("Run first script");
-},{}],"jFrn":[function(require,module,exports) {
-var o=20;console.log(o),console.log(l);var l=10;console.log(o);
+},{}],"d4kj":[function(require,module,exports) {
+var o={firstName:"bob",showName:function(){console.log(this.firstName)}},s=function(o){o()};s(o.showName.bind(o)),s(o.showName);
 },{}],"Focm":[function(require,module,exports) {
-"use strict";var e=s(require("./js/scriptOne")),r=s(require("./js/taskTwo"));function s(e){return e&&e.__esModule?e:{default:e}}
-},{"./js/scriptOne":"GjNk","./js/taskTwo":"jFrn"}]},{},["Focm"], null)
-//# sourceMappingURL=/my-trial-repository/src.9595ac3a.js.map
+"use strict";var e=s(require("./js/scriptOne")),r=s(require("./js/taskFive"));function s(e){return e&&e.__esModule?e:{default:e}}
+},{"./js/scriptOne":"GjNk","./js/taskFive":"d4kj"}]},{},["Focm"], null)
+//# sourceMappingURL=/my-trial-repository/src.7caad0cb.js.map
